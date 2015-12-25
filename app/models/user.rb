58c035a028f6b80d45a97487181bc8f5f3e9919f
Joao Victor <jvflcin@gmail.com>
2015-12-25 19:03:28 -0300

    User model now validates alphanumeric passwords.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  #Validates that the password comes in an alphanumeric format.
+  validates_format_of  :password, with: /\A([a-zA-Z]|\d)*\z/
+  
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
